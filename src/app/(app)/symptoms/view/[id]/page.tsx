360314635a152a521adc236f95cf6f38fed62a0f
Ying Wang <wangying826@gmail.com>
2026-05-09 19:07:30 +0100

    fix: add generateStaticParams to dynamic route pages for Next.js static export

--- src/app/(app)/symptoms/view/[id]/page.tsx
+++ src/app/(app)/symptoms/view/[id]/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-export const dynamic = 'force-static'
+export function generateStaticParams() {
+  return []
+}
 
 import { useEffect, useState } from 'react'
 import { useRouter, useParams } from 'next/navigation'
